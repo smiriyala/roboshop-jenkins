0fa8c6c792f9e67f34bfec46f5a94c3c28393678
subbum <subbu@gmail.com>
2023-07-22 05:56:08 +0100

    added CI-Pipeline

--- vars.tf
+++ vars.tf
@@ -1,8 +1,18 @@
 variable "folders" {
-    default = ["infra"]
+    default = ["infra", "CI-Pipeline"]
 }
+
 variable "jobs"{
     default = [
         { name = "roboshop", folder = "infra", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+
+        { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "dispatch", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+  
     ]
 }
