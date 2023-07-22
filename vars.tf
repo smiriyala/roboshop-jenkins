45c64dae998d4fe3125b0ab7722af6ea3ed8b17d
subbum <subbu@gmail.com>
2023-07-22 06:45:18 +0100

    updated template to dynamic URL and Variables added to each component URL

--- vars.tf
+++ vars.tf
@@ -4,15 +4,15 @@ variable "folders" {
 
 variable "jobs"{
     default = [
-        { name = "roboshop", folder = "infra", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-
-        { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-        { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-        { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-        { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-        { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-        { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
-        { name = "dispatch", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/roboshop-infra"}
+        { name = "roboshop", folder = "infra", repo_url = "https://github.com/smiriyala/roboshop-infra" },
+        
+        { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/frontend" },
+        { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/cart" },
+        { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/catalogue" },
+        { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/user" },
+        { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/shipping" },
+        { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/payment" },
+        { name = "dispatch", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/dispatch" }
   
     ]
 }
