53d1f84b1a9991ae5b1ba730818f6e020a4de230
subbum <subbu@gmail.com>
2023-07-28 23:16:09 +0100

    roboshop job change to single job

--- vars.tf
+++ vars.tf
@@ -4,8 +4,7 @@ variable "folders" {
 
 variable "m-jobs"{
     default = [
-        { name = "roboshop", folder = "infra", repo_url = "https://github.com/smiriyala/roboshop-infra" },
-        
+    
         { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/frontend" },
         { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/cart" },
         { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/smiriyala/catalogue" },
@@ -18,7 +17,9 @@ variable "m-jobs"{
 }
 
 variable "s-jobs"{
-    default = []
+    default = [
+        { name = "roboshop", folder = "infra", repo_url = "https://github.com/smiriyala/roboshop-infra" }
+    ]
 }
 
 variable "force"{
