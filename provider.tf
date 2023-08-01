b29ef7b0245e94d5e618c63aadc4fff5ecf31be7
subbum <subbu@gmail.com>
2023-08-01 08:55:10 +0100

    Jenkins IP updated

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "jenkins" {
-    server_url = "http://44.201.222.119:8080/" 
+    server_url = "http://44.201.157.107:8080/" 
     username = data.aws_ssm_parameter.jenkins_user.value
     password = data.aws_ssm_parameter.jenkins_pass.value
 }
