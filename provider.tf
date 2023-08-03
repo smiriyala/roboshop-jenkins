d6b6ab0eac54df44d3299be7d3f6f5afd0008ece
subbum <subbu@gmail.com>
2023-08-03 11:57:33 +0100

    jenkins IP udpated

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "jenkins" {
-    server_url = "http://44.201.157.107:8080/" 
+    server_url = "http://54.84.119.77:8080/" 
     username = data.aws_ssm_parameter.jenkins_user.value
     password = data.aws_ssm_parameter.jenkins_pass.value
 }
